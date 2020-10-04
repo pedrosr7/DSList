fcf49bc7b37cd3b8ca5b99e2b4ca1b974ba3383e
Pedro Sánchez <pedro.sanchez@aiwin.es>
2020-10-04 16:55:37 +0200

    DSLIST-2 Diff row list

--- dslist/src/main/java/thevoid/whichbinds/dslist/DSListAdapter.kt
+++ dslist/src/main/java/thevoid/whichbinds/dslist/DSListAdapter.kt
@@ -4,15 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import arrow.core.extensions.list.foldable.exists
 
-class DSListViewHolder(view: View): RecyclerView.ViewHolder(view)
+class FlowViewHolder(view: View): RecyclerView.ViewHolder(view)
 
-class DSListAdapter<R,T> : RecyclerView.Adapter<DSListViewHolder>() {
+class DSListAdapter<R,T> : RecyclerView.Adapter<FlowViewHolder>() {
 
     val rows: MutableList<Row<R,T>> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DSListViewHolder =
-        DSListViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlowViewHolder =
+        FlowViewHolder(
             LayoutInflater
                 .from(parent.context)
                 .inflate(viewType, parent, false)
@@ -22,12 +23,14 @@ class DSListAdapter<R,T> : RecyclerView.Adapter<DSListViewHolder>() {
 
     override fun getItemCount(): Int = this.rows.size
 
-    override fun onBindViewHolder(holder: DSListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: FlowViewHolder, position: Int) {
         val row = this.rows[position]
         row.content?.let { row.bindView(it, holder.itemView) }
     }
 
     fun submitRows(rows: Row<R,T>){
+        if(this.rows.exists { it.id != null && it.id == rows.id  }) return
+
         this.rows.add(rows)
         notifyDataSetChanged()
     }
